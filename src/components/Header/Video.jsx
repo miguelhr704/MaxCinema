cc34b9aba49d157c793ee522d148ac0ce2dd5517
Miguel Hernandez <miguelagushreyes@gmail.com>
2022-04-05 18:08:50 -0500

    Se cambio el video de fondo por iframe

--- src/components/Header/Video.jsx
+++ src/components/Header/Video.jsx
@@ -2,12 +2,36 @@ import React from "react";
 import video from "../../images/fb.mp4";
 
 function Video() {
-  return (
-    <>
-      <div className="header__video">
+  const video = () => {
+    return (
+      <>
         <video autoPlay loop muted className="video">
           <source src={video} type="video/mp4" />
         </video>
+        <iframe
+          id="ytplayer"
+          type="text/html"
+          width="135%"
+          height="200%"
+          src="https://www.youtube.com/embed/QfYgcLuxS5Y?playlist=QfYgcLuxS5Y&amp;loop=1&amp;autoplay=1&amp;controls=0&amp;showinfo=0&amp;mute=1&amp;disablekb=1&amp;modestbranding=1"
+          frameborder="0"
+          allowfullscreen=""
+        ></iframe>
+      </>
+    );
+  };
+  return (
+    <>
+      <div className="header__video">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/QfYgcLuxS5Y?controls=0&autoplay=1&mute=1&playlist=QfYgcLuxS5Y&loop=1"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen="1"
+        ></iframe>
       </div>
     </>
   );
